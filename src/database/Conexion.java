0cbca7b886483bafa0e9a5a61040d29fb5f37241
neviller666 <n920097@gmail.com>
2023-05-23 22:22:10 -0500

    czxcczzxc

--- src/database/Conexion.java
+++ src/database/Conexion.java
@@ -12,7 +12,7 @@ public class Conexion {
     private final String URL="jdbc:mysql://localhost:3306/";
     private final String DB ="dbsistema";
     private final String USER="root";
-    private final String PASSWORD="";
+    private final String PASSWORD="123";
     
     public Connection cadena;
     
