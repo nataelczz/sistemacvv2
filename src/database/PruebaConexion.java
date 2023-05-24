bf2761f93ad3dbc967478cd6e26ffbc5ed7fb9e0
delmiro6262 <134113966+delmiro6262@users.noreply.github.com>
2023-05-23 20:59:26 -0500

    iniciandopruevadeeduardo1

---
+++ src/database/PruebaConexion.java
@@ -0,0 +1,22 @@
+
+package database;
+
+public class PruebaConexion {
+    
+    public static void main(String[] args) {
+        
+        Conexion con=Conexion.getInstancia();
+        con.conectar();
+        if(con.cadena!=null){
+        
+            System.out.println("Conectado");
+        
+        }
+        else{
+        
+            System.out.println("Desconectado");
+        }
+         
+    }    
+}
+//dddsdsd
