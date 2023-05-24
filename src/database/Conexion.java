a8597b9b5dfbce1463c623b8316c3987ef38958e
delmiro6262 <134113966+delmiro6262@users.noreply.github.com>
2023-05-23 21:04:26 -0500

    prueva del principal

--- src/database/Conexion.java
+++ src/database/Conexion.java
@@ -60,3 +60,4 @@ public class Conexion {
         return instancia;
     }
 }
+//gfgfgfgfgsdsdsdsdsdsdsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
